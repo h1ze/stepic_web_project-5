1f2ea68cd43ab0ca41ac7817f01eef52b9ee161c
dv-zheludkov <dv@zheludkov.com>
2016-02-25 15:49:29 +0300

    2

--- init.sh
+++
@@ -1,8 +0,0 @@
-
-# Apache
-unlink /etc/nginx/sites-enabled/default
-
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/stepic_webtech.conf
-
-# Gunicorn
-ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
