0947cda34ae74c1f743e89ed0381758c3d42f0b4
dv-zheludkov <dv@zheludkov.com>
2016-02-25 16:06:08 +0300

    3

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
-# from wsgiref.simple_server import make_server
+import urlparse
 
-def simple_app(environ, start_response):
+def application(env, start_response):
+    data = urlparse.parse_qsl(env["QUERY_STRING"], True)
+
+    result = ""
+
+    for key, value in data:
+        result += key + "=" + value + "\n"
+
     status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return [environ['QUERY_STRING'].replace('&', '\n')]
-    # return [u"This is hello wsgi app".encode('utf8')]
+    headers = [('Content-Type', 'text/plain')]
 
-# httpd = make_server('', 8000, simple_app)
-# print "Serving on port 8000..."
-# httpd.serve_forever()
+    start_response(status, headers)
+
+    return result
--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
-# sudo apt-get update
-sudo apt-get install -y nginx
-# mkdir -p /home/box/web/public/{img,css,js}
-# mkdir -p /home/box/web/{uploads,etc}
-
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo nginx -t && sudo /etc/init.d/nginx restart
-sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx start
+sudo ln -s /home/box/web/etc/gunicorn.py  /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
