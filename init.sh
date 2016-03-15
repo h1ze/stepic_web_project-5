7a54af24e3ea0a4529129fc2b4745980dc6a6b52
h1ze <burgov@mail.ru>
2016-03-15 22:18:40 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/stepic-web-django
 sudo /etc/init.d/gunicorn restart
 
+sudo sed -i 's/skip-external-locking/skip-external-locking\ninnodb_use_native_aio = 0/' /etc/mysql/my.cnf
 sudo /etc/init.d/mysql start  
 sudo mysql -uroot -e "create database stepic_web_db CHARACTER SET utf8 COLLATE utf8_general_ci;"
 sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web_db.* TO 'stepic_web'@'localhost' IDENTIFIED BY 'stepic_web_pass';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-sudo sed -i 's/skip-external-locking/skip-external-locking\ninnodb_use_native_aio = 0/' /etc/mysql/my.cnf
 sudo pip install django-autofixture
 sudo python /home/box/web/ask/manage.py syncdb
 sudo python /home/box/web/ask/manage.py loadtestdata qa.Question:40
