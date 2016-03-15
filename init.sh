0e530cebde8af49034c1ca52e62d8e542f5adee8
h1ze <burgov@mail.ru>
2016-03-15 19:46:18 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,10 @@ sudo /etc/init.d/gunicorn restart
 
 sudo mysql -uroot -e "create database stepic_web_db"
 sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web_db.* TO 'stepic_web'@'localhost' IDENTIFIED BY 'stepic_web_pass';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 sudo pip install django-autofixture
 sudo python /home/box/web/ask/manage.py syncdb
 sudo python /home/box/web/ask/manage.py loadtestdata qa.Question:40
 sudo python /home/box/web/ask/manage.py loadtestdata qa.Answer:40
+
 
